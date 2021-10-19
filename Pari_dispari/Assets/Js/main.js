8439299becd7360714452b6426ea84f7c4f186a6
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-10-19 18:26:45 +0200

    BONUS: add js docs functions
    aggiunta documentazione del codice di tutte le funzioni

--- Pari_dispari/Assets/Js/main.js
+++ Pari_dispari/Assets/Js/main.js
@@ -8,14 +8,27 @@ const numberUserDigit = Number(prompt("Scegli un numero da 1 a 5"));
 document.getElementById("user_digit").innerHTML = `hai scelto ${wordUserDigit} e il numero ${numberUserDigit}`
 
 // function randomNumberPc
+/**
+ * Questa funzione restituisce un numero random compreso tra due valori indicati in input
+ * @param {number} min - numero minimo dal quale partire
+ * @param {number} max - numero massimo al quale arrivare
+ * @returns {number} - restituisce un numero random
+ */
 function randomNumberPc(min, max) {
     return Math.floor(Math.random() * max - min + 1) + min;
     
 }
+
 let pcNumber = randomNumberPc(1, 5)
 document.getElementById("pc_number").innerHTML = `il numero random del pc è ${pcNumber}`
 
 // function sum
+/**
+ * Questa funzione restituisce la somma di due numeri indicati in input
+ * @param {number} num1 - primo numero da passare per fare l'operazione 
+ * @param {number} num2 - secondo numero da passare per fare l'operazione 
+ * @returns {number} - restituisce la somma dei due numeri passati per eseguire l'operazione
+ */
 function sum (num1, num2) {
     return num1 + num2
     
@@ -23,6 +36,12 @@ function sum (num1, num2) {
 let sommaUserAndPc = sum(numberUserDigit, pcNumber);
 
 // function sommaPariDispari
+/**
+ * Questa funzione restituisce il valore pari o dispari di una somma (numero) passata in input
+ * @param {number} somma - indica la somma di due numeri da passare in input
+ * @returns {string} - restituisce la stringa "pari" se la condizione risulta vera. Restituisce la stringa "dispari" se la condizione non si verifica
+ * 
+ */
 function sommaPariDispari(somma) {
     if (somma % 2 == 0) {
     return "pari";
