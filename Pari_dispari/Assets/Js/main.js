0dc119ed2b32b4fe3291502a2a0d5c82bb99ec67
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-10-19 16:37:45 +0200

    add function randomNumberPc

--- Pari_dispari/Assets/Js/main.js
+++ Pari_dispari/Assets/Js/main.js
@@ -4,16 +4,25 @@
 // inizializzazione delle variabili
 const wordUserDigit = prompt("Scegli pari o dispari");
 const numberUserDigit = Number(prompt("Scegli un numero da 1 a 5"));
-const numberPc = Math.floor(Math.random() * 5) + 1;
-console.log(numberPc);
+console.log("il numero digitato dall'utente è " + numberUserDigit);
+/* const numberPc = Math.floor(Math.random() * 5) + 1;
+console.log(numberPc); */
 
-let sum = numberUserDigit + numberPc
-console.log(sum);
+/* let sum = numberUserDigit + numberPc
+console.log(sum); */
 
 
 // condzione di verifica
-if (sum % 2 == 0) {
+/* if (sum % 2 == 0) {
     console.log("la somma è pari");
 } else {
      console.log("la somma è dispari");
+} */
+
+function randomNumberPc(min, max) {
+    return Math.floor(Math.random() * max - min + 1) + min;
+    
 }
+/* console.log(randomNumberPc(1, 5)); */
+let pcNumber = randomNumberPc(1, 5)
+console.log("il numero random del pc è " + pcNumber);
