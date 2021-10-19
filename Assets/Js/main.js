c19ef7a96d7ad6ec4fd7c5c70f77c0eb3bd9d04b
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-10-19 15:14:03 +0200

    Add details js file
    aggiunto prompt e condizionale ancora senza funzione

--- Assets/Js/main.js
+++ Assets/Js/main.js
@@ -1,1 +1,25 @@
-// Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+// Chiedere all’utente di inserire una parola. Creare una funzione per capire se la parola inserita è palindroma
+
+/* Strumenti
+-prompt
+-if/else
+-function()
+*/
+
+
+// inizializzazione delle variabili
+const userDigitWord = prompt("Digita una parola");
+/* console.log(userDigitWord); */
+
+const palindromeWord = userDigitWord.split("").reverse().join("");
+
+/* console.log(palindromeWord); */
+
+// condizione di verifica
+if (userDigitWord == palindromeWord) {
+    console.log("la parola inserita è palindroma");
+
+} else {
+    console.log("la parola inserita NON è palindroma");
+
+}
