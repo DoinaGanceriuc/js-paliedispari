4377e46b0a59a22a6b95561b14235ed774ae5d7e
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-10-19 18:32:35 +0200

    BONUS: add js docs function
    aggiunta documentazione del codice alla funzione

--- Palindroma/Assets/Js/main.js
+++ Palindroma/Assets/Js/main.js
@@ -22,6 +22,11 @@ const palindromeWord = userDigitWord.split("").reverse().join("");
 } */
 
 // definizione di una funzione
+/**
+ * Questa funzione restituisce il risultato di una parola palindroma
+ * @param {string} word - indica una stringa da passare in input
+ * @returns {boolean} - restituisce il valore boolean true se la condizione risulta vera. Altrimenti restituisce il valore boolean false se la condizione non si verifica
+ */
 function palindrome (word) {
     if (word == palindromeWord) {
     return true;
